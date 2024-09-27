897a52b02c9ee36fa364265e5df712937ba21c20
Romeshika <157833846+RomeshikaDewmini@users.noreply.github.com>
2024-09-28 01:02:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
