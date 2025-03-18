a2c331ea2bac0d5cf77652fb50901de62bad1727
Romeshika <157833846+RomeshikaDewmini@users.noreply.github.com>
2025-03-18 22:24:23 +0530

    commit-4

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -6,7 +6,7 @@ export default function TodoInput(props) {
         <header>
             <input value={todoValue} onChange={(e) => {
                 setTodoValue(e.target.value)
-            }} placeholder= "Enter todo...." />
+            }} placeholder= "Enter todo.. .." />
             <button onClick={() => {
                 handleAddTodos(todoValue)
                 setTodoValue('')
